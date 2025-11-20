8f8cbf78209de620d4ac8793794e561536713061
Feihuan Peng <feihuan.peng@spreetail.com>
2025-11-20 01:30:41 -0600

    asd

--- app/src/main/java/com/example/activitylifecycleplayground/MainActivity.kt
+++ app/src/main/java/com/example/activitylifecycleplayground/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.activitylifecycleplayground
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,8 +13,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.button_exit).setOnClickListener {
+            Log.d("Feihuan", "in button click listener!!")
+            finish()
+        }
+        Log.d("Feihuan", "This is OnCreate!!")
     }
 
-
+    override fun onDestroy() {
+        Log.d("Feihuan", "This is OnDestroy!!")
+        super.onDestroy()
+    }
 
 }
