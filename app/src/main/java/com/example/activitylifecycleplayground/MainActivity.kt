c34f0b864f6b2a6e19a541525f120875b1f559e0
Feihuan Peng <feihuan.peng@spreetail.com>
2025-11-21 00:31:54 -0600

    asd

--- app/src/main/java/com/example/activitylifecycleplayground/MainActivity.kt
+++ app/src/main/java/com/example/activitylifecycleplayground/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.activitylifecycleplayground
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -8,43 +9,27 @@ import com.example.activitylifecycleplayground.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var isFirstLoad = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.buttonExit.setOnClickListener {
-            Log.d("Feihuan", "in button click listener!!")
-            startActivity(Intent(this, ANonFullScreenActivity::class.java))
+            finish()
         }
-        Log.d("Feihuan", "This is OnCreate!!")
     }
-//
-//    override fun onStart() {
-//        super.onStart()
-//        Log.d("Feihuan", "This is OnStart!!")
-//    }
-//
-//    // usually refresh data
-//    override fun onResume() {
-//        super.onResume()
-//        Log.d("Feihuan", "This is OnResume!!")
-//    }
-//
-//    override fun onPause() {
-//        super.onPause()
-//        Log.d("Feihuan", "This is OnPause!!")
-//    }
-//
-//    override fun onStop() {
-//        super.onStop()
-//        Log.d("Feihuan", "This is OnStop!!")
-//    }
-//
-//    // onDestroy is not guarenteed to run
-//    override fun onDestroy() {
-//        Log.d("Feihuan", "This is OnDestroy!!")
-//        super.onDestroy()
-//    }
+    // No 1 place for refreshing data
+    override fun onResume() {
+        super.onResume()
+        if(isFirstLoad){
+            binding.textViewRefreshStatus.text = "welcome to the App, Here is your feed..."
+            isFirstLoad = false
+        } else {
+            binding.textViewRefreshStatus.text = "updated"
+        }
+    }
+
+
 
 }
