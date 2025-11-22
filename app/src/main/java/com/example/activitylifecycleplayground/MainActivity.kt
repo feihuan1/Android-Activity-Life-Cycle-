d88fff09401d4e94c9c86e7329c039fb5ac105a7
Feihuan Peng <feihuan.peng@spreetail.com>
2025-11-22 01:55:32 -0600

    asd

--- app/src/main/java/com/example/activitylifecycleplayground/MainActivity.kt
+++ app/src/main/java/com/example/activitylifecycleplayground/MainActivity.kt
@@ -9,27 +9,29 @@ import com.example.activitylifecycleplayground.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var isFirstLoad = true
+//    private var isFirstLoad = true
+    private var numberOfLoads = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.textViewRefreshStatus.text = "welcome to the App, Here is your feed..."
+
         binding.buttonExit.setOnClickListener {
             finish()
         }
     }
-    // No 1 place for refreshing data
+//     No 1 place for refreshing data
     override fun onResume() {
         super.onResume()
-        if(isFirstLoad){
-            binding.textViewRefreshStatus.text = "welcome to the App, Here is your feed..."
-            isFirstLoad = false
-        } else {
-            binding.textViewRefreshStatus.text = "updated"
-        }
+        numberOfLoads += 1
     }
 
+    override fun onRestart() {
+        super.onRestart()
+        binding.textViewRefreshStatus.text = "updated $numberOfLoads times"
+    }
 
 
 }
