1261affba40e7c01ce9dafa500ceeb4ec3948293
Feihuan Peng <feihuan.peng@spreetail.com>
2025-11-23 23:03:01 -0600

    asd

--- app/src/main/java/com/example/activitylifecycleplayground/MainActivity.kt
+++ app/src/main/java/com/example/activitylifecycleplayground/MainActivity.kt
@@ -1,37 +1,25 @@
 package com.example.activitylifecycleplayground
 
-import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.activitylifecycleplayground.databinding.ActivityMainBinding
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-//    private var isFirstLoad = true
-    private var numberOfLoads = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.textViewRefreshStatus.text = "welcome to the App, Here is your feed..."
-
         binding.buttonExit.setOnClickListener {
             finish()
         }
     }
-//     No 1 place for refreshing data
-    override fun onResume() {
-        super.onResume()
-        numberOfLoads += 1
+
+    override fun onDestroy() {
+        super.onDestroy()
+        val userMessage = binding.editTextMessage.text
+        File(filesDir, "user message.txt").writeText(userMessage.toString())
     }
-
-    override fun onRestart() {
-        super.onRestart()
-        binding.textViewRefreshStatus.text = "updated $numberOfLoads times"
-    }
-
-
 }
