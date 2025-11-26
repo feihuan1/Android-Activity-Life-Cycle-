0815b414c8904f96095dd002631381ff60e39b6b
Feihuan Peng <feihuan.peng@spreetail.com>
2025-11-25 18:36:17 -0600

    asd

--- app/src/main/java/com/example/activitylifecycleplayground/MainActivity.kt
+++ app/src/main/java/com/example/activitylifecycleplayground/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.example.activitylifecycleplayground
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.activitylifecycleplayground.databinding.ActivityMainBinding
-import java.io.File
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -13,13 +14,32 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.buttonExit.setOnClickListener {
-            finish()
+            showDialog()
         }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        val userMessage = binding.editTextMessage.text
-        File(filesDir, "user message.txt").writeText(userMessage.toString())
+//    override fun onDestroy() {
+//        super.onDestroy()
+//        val userMessage = binding.editTextMessage.text
+//        File(filesDir, "user message.txt").writeText(userMessage.toString())
+//    }
+
+    private fun showDialog(){
+        AlertDialog.Builder(this)
+            .setTitle("Warning")
+//            .setMessage("Are you sure you want exit the app!!!")
+            .setView(R.layout.dialog_warning)
+            .setPositiveButton("yes") {_, _ ->
+                finish()
+            }
+            .setNegativeButton("No") {dialog,_ ->
+                dialog.dismiss()
+            }
+            .setNeutralButton("Neutral"){_,_,->
+                Toast.makeText(this, "this is where the more info screen could be", Toast.LENGTH_LONG).show()
+
+            }
+            .show()
+
     }
 }
